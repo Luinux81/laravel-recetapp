a5b648fccdda93bf5a6afe18fad0ff406e657c2f
Luis BC <luinux81@gmail.com>
2021-10-01 18:09:47 +0200

    Housekeeping

--- dev/resources/views/dashboard.blade.php
+++ dev/resources/views/dashboard.blade.php
@@ -9,16 +9,6 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 {{-- <x-jet-welcome /> --}}
-
-                <x-tabs.tabs data-id="tabs-1" activa="tab1">
-                    <x-tabs.tab nombre="tab1" titulo="La Tab 1" icono="fas-cloud">
-                        Esto es la tab
-                    </x-tabs.tab>
-                    <x-tabs.tab nombre="tab2" titulo="La Tab 2" icono="fas-camera">
-                        Esto es la tab 2
-                    </x-tabs.tab>
-                </x-tabs.tabs>
-
             </div>
         </div>
     </div>
