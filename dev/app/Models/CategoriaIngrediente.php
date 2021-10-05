710af7cdccd979cb86119036f2a8550c87e605a6
Luis BC <luinux81@gmail.com>
2021-10-06 00:19:56 +0200

    Fix: Obtención correcta de categorias de ingrediente hijas

--- dev/app/Models/CategoriaIngrediente.php
+++ dev/app/Models/CategoriaIngrediente.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Models\User;
 use App\Models\Ingrediente;
+use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -27,11 +28,37 @@ class CategoriaIngrediente extends Model
         return $this->belongsTo(CategoriaIngrediente::class);
     }
 
-    public function categoriaHijas(){
-        return $this->hasMany(CategoriaIngrediente::class,"catParent_id");
+    public function categoriasHija($recursivo = false){
+        $out = new Collection();
+        
+        $hijas = CategoriaIngrediente::where('catParent_id','=',$this->id)->get();        
+        $visitados = collect();        
+        $i = 0;
+
+        if($recursivo){            
+            while($i < $hijas->count()){
+                $cat = $hijas->all()[$i];
+                if (!$visitados->contains($cat)){
+                    $visitados->add($cat);
+
+                    $subHijas = CategoriaIngrediente::where('catParent_id','=',$cat->id)->get();
+                    foreach ($subHijas as $key => $subHija) {
+                        $hijas->add($subHija);
+                    }
+                }
+                $i++;
+            }  
+            $out = $hijas;      
+        }
+        else{
+            $out = $hijas;
+        }
+
+        return $out;
     }
 
     public function categoriaRaiz(){
 
     }
 }
+
