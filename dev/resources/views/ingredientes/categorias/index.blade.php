520424b95bb4a0f1bbc2d0e9ed99aab19a92faa4
Luis BC <luinux81@gmail.com>
2021-10-05 19:50:09 +0200

    Extracción de componente blade para contenido

--- dev/resources/views/ingredientes/categorias/index.blade.php
+++ dev/resources/views/ingredientes/categorias/index.blade.php
@@ -1,32 +1,35 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Categorias de ingredientes') }}
-        </h2>
+        <div class="flex justify-between align-top">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Categorias de ingredientes') }}
+            </h2>
+            <a class="boton boton--azul" href='{{ route('ingredientes.categoria.create') }}'>Nuevo</a>
+        </div>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                Esto es la vista de categorias de ingredientes
-
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Nombre</th>
-                            <th>Descripcion</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    @foreach ($categorias as $c)
-                        <tr>
-                            <td>{{$c->nombre}}</td>
-                            <td>{{$c->descripcion}}</td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
+    <x-content>
+        <table>
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Descripcion</th>
+                    <th>Categoria Superior</th>
+                </tr>
+            </thead>
+            <tbody>
+            @foreach ($categorias as $c)
+                <tr>
+                    <td>{{$c->nombre}}</td>
+                    <td>{{$c->descripcion}}</td>
+                    <td>
+                        @if (!empty($c->catParent_id))
+                            {{ \App\Models\CategoriaIngrediente::find($c->catParent_id)->nombre }}
+                        @endif
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </x-content>
 </x-app-layout>
