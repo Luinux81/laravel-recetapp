dc994664182fa929db670ff4d50bfa76ccd2f1e4
Luis BC <luinux81@gmail.com>
2021-12-12 19:43:14 +0100

    Rutas y configuración API

--- dev/routes/api.php
+++ dev/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\IngredienteController;
+use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register',[AuthController::class, 'register']);
+Route::post('/login',[AuthController::class, 'login']);
+
+Route::group(['middleware' => 'auth:sanctum'], function(){
+    Route::post('/logout',[AuthController::class, 'logout']);
+
+    Route::prefix("v1")->name("v1.")->group(function(){
+        Route::apiResource("ingredientes",IngredienteController::class);
+    });
+    
+});
+
