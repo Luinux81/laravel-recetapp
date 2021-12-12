371c5677dab3ae0607f408aff9ecef1c61ef57a2
Luis BC <luinux81@gmail.com>
2021-12-12 12:58:41 +0100

    Limpieza de comentarios

--- dev/app/Models/Ingrediente.php
+++ dev/app/Models/Ingrediente.php
@@ -14,30 +14,6 @@ class Ingrediente extends Model
     use HasFactory;
     use SoftDeletes;
 
-    // public $user_id;
-    // public $cat_id;
-
-    // public $nombre;
-    // public $descripcion;
-    // public $marca ;
-    // public $barcode;
-    // public $imagen;
-    // public $url;
-        
-    // public $calorias;
-    // public $fat_total;
-    // public $fat_saturadas;
-    // public $fat_poliinsaturadas;
-    // public $fat_monoinsaturadas;
-    // public $fat_trans;
-    // public $colesterol;
-    // public $sodio;
-    // public $potasio;
-    // public $fibra;
-    // public $carb_total;
-    // public $carb_azucar;
-    // public $proteina;
-
     protected $guarded = [];
 
 
