8603d03c01b466f1b822da5b5c9b8af0ecb1f4e9
Luis BC <luinux81@gmail.com>
2022-01-02 17:45:29 +0100

    Funcionalidad: Obtención de datos de ingrediente desde OpenFoodFacts a partir de código de barras

--- dev/resources/views/dashboard.blade.php
+++ dev/resources/views/dashboard.blade.php
@@ -1,3 +1,8 @@
+@php
+    use \App\Helpers\OpenFoodFacts;   
+    use \App\Models\Ingrediente; 
+@endphp
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -7,8 +12,11 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                {{-- <x-jet-welcome /> --}}
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
+                {{-- {{ 
+                    //dd(OpenFoodFacts::getProductoOFFByCode(OpenFoodFacts::$dataset["crema_avellana"])));
+                    //dd(Ingrediente::getIngredienteOpenFoodFact(OpenFoodFacts::$dataset["donuts"]));                    
+                }} --}}
             </div>
         </div>
     </div>
