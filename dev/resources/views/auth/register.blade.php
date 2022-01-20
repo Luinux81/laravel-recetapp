d209d4ba8d0ea80c404e470de37f215f840ce778
Luis BC <luinux81@gmail.com>
2022-01-20 19:57:13 +0100

    Fix: UI registro

--- dev/resources/views/auth/register.blade.php
+++ dev/resources/views/auth/register.blade.php
@@ -46,7 +46,7 @@
                     </div>
                 @endif
 
-                <div class="flex items-center justify-end mt-4">
+                <div class="flex-column items-center justify-end mt-4">
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
                         {{ __('Ya estás registrado?') }}
                     </a>
