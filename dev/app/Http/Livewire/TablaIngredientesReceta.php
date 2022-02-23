10f7f0005730b2bf2648388739e73f3cda0f6d9c
Luis BC <luinux81@gmail.com>
2022-02-23 12:27:04 +0100

    Funcionalidad para añadir nuevo ingrediente a receta

--- dev/app/Http/Livewire/TablaIngredientesReceta.php
+++ dev/app/Http/Livewire/TablaIngredientesReceta.php
@@ -2,12 +2,30 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Ingrediente;
 use App\Models\Receta;
 use Livewire\Component;
 
 class TablaIngredientesReceta extends Component
 {
     public $receta;
+
+    public $creando_new = false;
+    public $new_ingrediente_cantidad;
+    public $new_ingrediente_unidad;
+    public $new_ingrediente_id;
+
+    protected $listeners = [
+        'refreshTabla' => '$refresh',
+        'addIngredienteReceta'=>'addIngredienteReceta',
+        'clearNewFormErrors'
+    ];
+
+    protected $rules = [
+        'new_ingrediente_cantidad' => 'required',
+        'new_ingrediente_unidad' => 'required',
+        'new_ingrediente_id' => 'required',
+    ];
 
     public function mount(Receta $receta)
     {
@@ -17,5 +35,36 @@ class TablaIngredientesReceta extends Component
     public function render()
     {
         return view('livewire.tabla-ingredientes-receta');
+    }
+    
+    public function addIngredienteReceta($payload)
+    {
+        $this->new_ingrediente_cantidad = $payload["cantidad"];
+        $this->new_ingrediente_unidad = $payload["unidad"];
+        $this->new_ingrediente_id = $payload["ingrediente"];
+
+        $this->validate();
+
+        $ingrediente = Ingrediente::find($this->new_ingrediente_id);
+
+        $this->receta
+            ->ingredientes()
+            ->attach($ingrediente, [
+                    'cantidad' => $this->new_ingrediente_cantidad, 
+                    'unidad_medida' => $this->new_ingrediente_unidad
+                ]);
+        
+        $this->emitSelf('refreshTabla');
+        $this->creando_new = false;
+    }
+
+    public function clearNewFormErrors()
+    {
+        $this->resetErrorBag();
+    }
+
+    public function setCreandoNew($valor)
+    {
+        $this->creando_new = $valor;
     }
 }
--- dev/resources/views/livewire/tabla-ingredientes-receta.blade.php
+++ dev/resources/views/livewire/tabla-ingredientes-receta.blade.php
@@ -1,10 +1,51 @@
-<div>
+@push('custom-scripts')
+    <script>
+        function visibilizaNewIngrediente(visible){
+            const row = document.getElementById('new-ingrediente');
+
+            if(visible){
+                row.classList.remove('invisible');
+            }
+            else{
+                row.classList.add('invisible');
+            }
+        }
+
+        function cancelarNew(){
+            visibilizaNewIngrediente(false);
+            document.getElementById("new-ingrediente-cantidad").value="";
+            document.getElementById("new-ingrediente-unidad").value="";
+            document.getElementById("new-ingrediente-nombre").value="";
+            Livewire.emit('clearSearchBox');
+            Livewire.emit('clearNewFormErrors');
+        }
+
+        function submitNew(){
+            const cantidad = document.getElementById("new-ingrediente-cantidad").value;
+            const unidad = document.getElementById("new-ingrediente-unidad").value;
+            const id_ingrediente = document.getElementById("new-ingrediente-nombre").value;
+
+            Livewire.emit('addIngredienteReceta',{cantidad:cantidad,unidad:unidad,ingrediente:id_ingrediente});
+        }
+
+    </script>
+@endpush
+
+<div class="relative">
+    <div class="full__overlay invisible" wire:loading.class.remove="invisible">
+        <div class="loader">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
     <table class="w-full" style="border-collapse:separate;border-spacing:0 .3em;">
         <thead>
             <tr>
                 <th class="w-1/12 text-center">Cantidad</th>
-                <th class="w-1/12 text-center">Unidades</th>
-                <th class="w-7/12 text-left">Nombre</th>
+                <th class="w-2/12 text-center">Unidades</th>
+                <th class="w-6/12 text-left">Nombre</th>
                 <th class="w-3/12 text-center">Acciones</th>
             </tr>
         </thead>
@@ -34,30 +75,46 @@
 
                 </tr>
             @endforeach
-            <tr id="new-ingrediente" class="invisible bg-gray-400">
+            <tr id="new-ingrediente" class="@if($creando_new == false) invisible @endif bg-gray-400">
                 <td>
-                    <div class="flex flex-col justify-center">
-                        <label for="new-ingrediente-cantidad">Cantidad</label>
-                        <input id="new-ingrediente-cantidad" type="text" class="form-input bg-gray-200 rounded-md mb-3"/>
+                    <div class="flex flex-col justify-start">
+                        <label for="new-ingrediente-cantidad">
+                            Cantidad @error('new_ingrediente_cantidad') <span style="color:red;">*</span> @enderror
+                        </label>
+                        <input id="new-ingrediente-cantidad" type="text" class="form-input bg-gray-200 rounded-md mb-3" maxlenght="6" size="6"/>
                     </div>
                 </td>
                 <td>
                     <div class="flex flex-col justify-center">
-                        <label for="new-ingrediente-unidad">Unidades</label>
-                        <input id="new-ingrediente-unidad" type="text" class="form-input bg-gray-200 rounded-md mb-3"/>
+                        <label for="new-ingrediente-unidad">
+                            Unidades @error('new_ingrediente_unidad') <span style="color:red;">*</span> @enderror
+                        </label>
+                        <input id="new-ingrediente-unidad" type="text" class="form-input bg-gray-200 rounded-md mb-3" maxlenght="10" size="10"/>
                     </div>
                 </td>
                 <td colspan="2">
                     <div class="flex">
                         <div class="flex flex-col grow" style="flex-grow:1;">
-                            <label for="new-ingrediente-Nombre">Nombre</label>
-                            <input id="new-ingrediente-Nombre" type="text" class="w-full form-input bg-gray-200 rounded-md mb-3"/>
+                            <label for="new-ingrediente-nombre">
+                                Nombre @error('new_ingrediente_id') <span style="color:red;">*</span> @enderror
+                            </label>
+                            {{-- <input id="new-ingrediente-nombre" type="text" class="w-full form-input bg-gray-200 rounded-md mb-3"/> --}}
+                            @livewire('search-box',["clase"=>"ingrediente","nombre"=>"new-ingrediente-nombre"],key('new-search-box'))
+                            
                         </div>
                         <div class="flex items-center gap-3 mx-4">
-                            <button class="boton boton--icono boton--verde">
+                            <button 
+                                class="boton boton--icono boton--verde"
+                                wire:click="setCreandoNew(true)"
+                                onclick="submitNew();"
+                                >
                                 <x-fas-check class="icono--boton-2"></x-fas-check>
                             </button>
-                            <button class="boton boton--icono boton--rojo">
+                            <button 
+                                class="boton boton--icono boton--rojo"
+                                wire:click="setCreandoNew(false)"
+                                onclick="cancelarNew();"
+                            >
                                 <x-fas-times class="icono--boton-2"></x-fas-times>
                             </button>
                         </div>
@@ -67,12 +124,12 @@
         </tbody>
     </table>
 
-    <a 
-        href="#" 
-        class="boton boton--azul my-10 w-60"
-        wire:click.prevent = ""
+    <button 
+        
+        class="boton boton--azul my-10 w-60"        
+        onclick="visibilizaNewIngrediente(true)"
     >
         <x-fas-plus class="icono--boton-1"></x-fas-plus>
         <span>Añadir ingrediente</span>
-    </a>
+    </button>
 </div>
